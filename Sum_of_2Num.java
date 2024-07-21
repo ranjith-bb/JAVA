114901d7b46873d525db3b127fdb55563d1262a1
ranjith-bb <ranjithbb282005@gmail.com>
2024-07-21 20:46:27 +0530

    Create Sum_of_2Num.java

---
+++ Sum_of_2Num.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        scanner.close();
+    }
+}
